1788cb24d63f55c39c7b3610bc40bd9051818151
dacha2207 <dachaj2207@gmail.com>
2023-06-28 02:51:52 +0200

    Implemented add button

--- gui.py
+++ gui.py
@@ -1,14 +1,31 @@
 import functions
 import PySimpleGUI
 
+
+
 label = PySimpleGUI.Text("Type in a todo:")
-input_box = PySimpleGUI.InputText(tooltip="Enter todo")
+input_box = PySimpleGUI.InputText(tooltip="Enter todo",key="todo")
 add_button = PySimpleGUI.Button("Add")
 edit_button = PySimpleGUI.Button("Edit")
 complete_button = PySimpleGUI.Button("Complete")
 exit_button = PySimpleGUI.Button("Exit")
 
 
-window = PySimpleGUI.Window('My todo app', layout=[[label], [input_box,add_button]])
-window.read()
+window = PySimpleGUI.Window('My todo app',
+                            layout=[[label], [input_box,add_button]],
+                            font=('Helvetica',16))
+while True:
+    event, values = window.read()
+    print(event)
+    print(values)
+    match event:
+        case "Add":
+            todos = functions.get_todos()
+            new_todo = values['todo'] + "\n"
+            todos.append(new_todo)
+            functions.write_todos(todos)
+        case PySimpleGUI.WIN_CLOSED:
+            break
+
+
 window.close()
